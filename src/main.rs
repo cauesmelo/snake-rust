415432a62267cc65b72293360a9c87aaf63f334d
Cauê Melo <cauesmelo@gmail.com>
2021-09-25 22:47:09 -0300

    feat: add draw bug

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 use macroquad::prelude::*;
+mod colors;
+mod snake;
 
-type Point = (i16, i16);
-
-struct Snake {
-    pos: Point,
-}
+static GRID_SIZE: i16 = 30;
 
 #[macroquad::main("Snake")]
 async fn main() {
-    let mut snake = Snake { pos: (0, 0) };
+    let snake = snake::Snake { head: (0, 0) };
+    let pixel_size: f32 = 10.0;
 
     loop {
-        clear_background(GREEN);
+        clear_background(colors::GREEN);
 
-        draw_rectangle(0.0, 0.0, 20.0, 20.0, BLACK);
+        draw_rectangle(
+            snake.head.0 as f32 * pixel_size,
+            snake.head.1 as f32 * pixel_size,
+            20.0,
+            20.0,
+            colors::BLACK,
+        );
+
+        draw_debug();
 
         next_frame().await
     }
+}
+
+fn draw_debug() {
+    let window_size = format!("Window size: {}, {}", screen_width(), screen_height());
+    let fps = format!("FPS: {}", get_fps());
+
+    draw_text(&window_size, 10.0, screen_height() - 50.0, 26.0, BLACK);
+    draw_text(&fps, 10.0, screen_height() - 20.0, 26.0, BLACK);
 }
